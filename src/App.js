dd34c12d2560eb5fef16651ba7c4d265424eaa60
MuntazirHaider <muntazirhaiderc320@gmail.com>
2023-03-05 09:16:25 +0530

    Deploy app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Button } from "react-bootstrap";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is a ecom project</h1>
+      <Button>Bootstrap buton</Button>
+    </div>
+  );
+}
+
+export default App;
